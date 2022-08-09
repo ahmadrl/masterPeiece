65494c4025d41352adf8b7a5808a38f80f8352c4
AhmadKhalaf <ahmad.alkhalafse@gmail.com>
2022-08-09 21:31:08 +0200

    main

---
+++ Websteder/resources/views/frontend/pages/about-us.blade.php
@@ -0,0 +1,87 @@
+@extends('frontend.layouts.master')
+
+@section('title','WEBSTEDER || About Us')
+
+@section('main-content')
+
+	<!-- Breadcrumbs -->
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="bread-inner">
+						<ul class="bread-list">
+							<li><a href="index1.html">Home<i class="ti-arrow-right"></i></a></li>
+							<li class="active"><a href="blog-single.html">About Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Breadcrumbs -->
+
+	<!-- About Us -->
+	<section class="about-us section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-12">
+						<div class="about-content">
+							
+							<h3>Welcome To <span>WEBSTEDER</span></h3>
+							<p>websteder one of the tbest marketplace for sell and buy websites,<br/>
+							best quality websites for personal and commercial use.
+							choose your website then start your bussiness
+							</p>
+							<div class="button">
+								<a href="{{route('product-grids')}}" class="btn primary">Shop Now</a>
+							</div>
+						</div> 
+					</div>
+					<div class="col-lg-6 col-12">
+						<div class="about-img overlay">
+							
+							<img src="{{asset('backend/img/Multivendor-Marketplace-Mistakes.webp')}}">
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- End About Us -->
+
+
+	<!-- Start Shop Services Area -->
+	<section class="shop-services section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					
+					<!-- End Single Service -->
+				</div>
+				
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-lock"></i>
+						<h4>Sucure Payment</h4>
+						<p>100% secure payment</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-tag"></i>
+						<h4>Best Products</h4>
+						<p>Guaranteed price</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Shop Services Area -->
+
+	@include('frontend.layouts.newsletter')
+@endsection
